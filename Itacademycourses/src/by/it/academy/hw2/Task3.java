f60bda7d8d262ce16f963ba2f0b47287563b64f0
Lebedik <lebedik550@gmail.com>
2023-02-09 15:14:26 +0300

    Add correct homework for task2

--- Itacademycourses/src/by/it/academy/hw2/Task3.java
+++ Itacademycourses/src/by/it/academy/hw2/Task3.java
@@ -6,13 +6,21 @@ public class Task3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter number of seconds" );
-        int number = in.nextInt();
-        int weeks = number / (3600 * 24 * 7);
-        int days = number / (3600 * 24) % 7;
-        int hours = (number - weeks * 3600 * 24 * 7 - days * 3600 * 24 ) / 3600;
-        int minute = (number - weeks * 3600 * 24 * 7 - days * 3600 * 24 - days * 3600 ) / 60;
-        int seconds = number % 60;
-        System.out.println("Weeks = " + weeks +"\nDays = " + days +"\nHours = " + hours +"\nMinutes = " + minute +"\nSeconds = " + seconds);
+        int x = in.nextInt();
+        int m = 60;
+        int h = 3600;
+        int d = 86400;
+        int w = 604800;
+
+        int weeks = x/w;
+        int days = x/d-weeks*7;
+        int hours = x/h-days*24-weeks*7*24;
+        int minutes = x/m-hours*60-days*24*60-weeks*7*24*60;
+        int seconds = x-minutes*60-hours*60*60-days*24*60*60-weeks*7*24*60*60;
+
+
+
+        System.out.println("Weeks: " + weeks +"\nDays: " + days +"\nHours: " + hours +"\nMinutes: " + minutes +"\nSeconds: " + seconds);
     }
 }
 
